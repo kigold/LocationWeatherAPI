612913da6a44b562bbd76a7bfff704cd1ea7142c
kingsley <kingsleybox@yaho.com>
2019-03-26 17:03:05 +0100

    implemented Weather Display

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import getWeather from "./weather"
-console.log("Starting ...")
 const locations = ["London,uk", "Lagos,nigeria"]
 const postalCodes = ["101233", "101222"]
 for(let i = 0; i < locations.length; i++){
@@ -8,9 +7,16 @@ for(let i = 0; i < locations.length; i++){
     })
 }
 
-
-
 const displayWeather =  (weatherResponse) => {
-    console.log(weatherResponse)
-    
+    let response = JSON.parse(weatherResponse)
+    if(response.cod == 200){
+        console.log("The weather report for  " + response.name)
+        const report = {
+            main : response.weather[0].main,
+            description : response.weather[0].description,
+            temperature : response.main.temp,
+            wind: response.wind.speed
+        }
+        console.log(report)     
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const weather_1 = require("./weather");
-console.log("Starting ...");
 const locations = ["London,uk", "Lagos,nigeria"];
 const postalCodes = ["101233", "101222"];
 for (let i = 0; i < locations.length; i++) {
@@ -10,6 +9,16 @@ for (let i = 0; i < locations.length; i++) {
     });
 }
 const displayWeather = (weatherResponse) => {
-    console.log(weatherResponse);
+    let response = JSON.parse(weatherResponse);
+    if (response.cod == 200) {
+        console.log("The weather report for  " + response.name);
+        const report = {
+            main: response.weather[0].main,
+            description: response.weather[0].description,
+            temperature: response.main.temp,
+            wind: response.wind.speed
+        };
+        console.log(report);
+    }
 };
 //# sourceMappingURL=index.js.map
