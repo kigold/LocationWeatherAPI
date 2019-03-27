12dfb0c5b794bccd9ce02a7e86aaea67a14ca7d7
kingsley <kingsleybox@yaho.com>
2019-03-27 15:57:25 +0100

    Refactor - Moved Code to src folder, and moved build to dist folder

--- src/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-let rawData = fs.readFileSync('config.json').toString();
-const config = JSON.parse(rawData);
-exports.default = config;
-//# sourceMappingURL=config.js.map
